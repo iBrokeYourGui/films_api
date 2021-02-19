2a2d714723f3df15caec68a2725764c4e8d1ee9d
archymail@ya.ru <archymail@ya.ru>
2021-02-19 23:13:12 +0300

    Add config for postge on heroku SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL' or 'sqlite:///' + str(BASE_DIR / "data" / "db.sqlite3"))

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+import os
 import pathlib
 
 BASE_DIR = pathlib.Path(__file__).parent
 
 
 class Config():
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / "data" / "db.sqlite3")
+    # os.environ.get('DATABASE_URL' - для подключения postgres на heroku
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL' or 'sqlite:///' + str(BASE_DIR / "data" / "db.sqlite3"))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Вот аткую строку лучше автогенераторами создать uuid например
     SECRET_KEY = 'you-will-never-know'
