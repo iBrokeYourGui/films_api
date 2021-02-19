ba36816d0f7ae04303e4cf5982b5fea246c2f4df
archymail@ya.ru <archymail@ya.ru>
2021-02-19 23:01:48 +0300

    fix port problems on heroku

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
+import os
+
 from src import app
 
 if __name__ == '__main__':
+    # эта строка нужно для публикации на хероку. Заменять захаржкоженый код на переменные окружения
+    port = int(os.environ.get('PORT', 8000))
     app.run(debug=True, host='0.0.0.0')
