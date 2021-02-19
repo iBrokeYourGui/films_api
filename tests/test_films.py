b898725ddf1c7ee272c16b9bf7937f1a21768feb
archymail@ya.ru <archymail@ya.ru>
2021-02-19 20:07:57 +0300

    fix some problems with pipenv lock pipenv lock --pre and fix config

--- tests/test_films.py
+++ tests/test_films.py
@@ -24,7 +24,7 @@ class TestFilms:
 
         assert resp.status_code == HTTPStatus.OK
 
-    @patch('src.services.film_service.FilmService.fetch_all_films', autospec=True)
+    @patch('src.services.film_service.FilmService.fetch_all_films')
     def test_get_films_mock_db(self, mock_db_call):
         client = app.test_client(db.session)
         resp = client.get('/films')
